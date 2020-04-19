671ca01cc89a2639cc5a5eef6c6ab3551c73079d
Steve Poll <stevepoll@gmail.com>
2020-04-19 17:51:37 -0400

    Completed Lesson 10 - Saving Stores and using Mixins

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,11 @@
 exports.homePage = (req, res) => {
-  res.render('index')
+  res.render('index', { title: 'Home' })
+}
+
+exports.addStore = (req, res) => {
+  res.render('addEditStore', { title: 'Add store' })
+}
+
+exports.createStore = (req, res) => {
+  res.json(req.body)
 }
