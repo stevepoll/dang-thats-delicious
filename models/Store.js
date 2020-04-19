095e7b2d3790094e7d1f5af3ef57f69d3be04adb
Steve Poll <stevepoll@gmail.com>
2020-04-19 15:06:07 -0400

    Completed Lesson 9 - Creating Store model

---
+++ models/Store.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+
+const slug = require('slugs')
+
+const storeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: 'Please enter a store name'
+  },
+  slug: String,
+  description: {
+    type: String,
+    trim: true
+  },
+  tags: [String]
+})
+
+storeSchema.pre('save', function(next) {
+  if (this.isModified('name')) {
+    this.slug = slug(this.name)
+    next()
+    // TODO: Make more resilient so slugs are unique
+  }
+})
+
+module.exports = mongoose.model('Store', storeSchema)
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const storeController = require('../controllers/storeController')
+
 // Do work here
-router.get('/', (req, res) => {
-  const phil = {name: 'Phil', age: 33, cool: true}
-  
-  // res.send('Hey! It works!');
-  // res.json(phil)
-  // res.send(req.query.name)
-  // res.json(req.query)
-
-  res.render('hello', {
-    name: 'Boo',
-    dog: 'Snickers',
-    title: 'I love food'
-  })
-});
-
-router.get('/reverse/:name', (req, res) => {
-  const rev = [...req.params.name].reverse().join('')
-  res.send(rev)
-})
+router.get('/', storeController.homePage)
 
 module.exports = router;
