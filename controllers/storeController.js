5bfe2dc4a3b205e3debc195d78dcbed06a17ab0c
Steve Poll <stevepoll@gmail.com>
2020-04-20 12:05:21 -0400

    Completed Lesson 14 - Creating an Editing Flow for Stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -19,3 +19,28 @@ exports.getStores = async (req, res) => {
   const stores = await Store.find()
   res.render('stores', { "title": "All stores", stores })
 }
+
+exports.editStore = async (req, res) => {
+  // 1. Find the store given the id
+  // res.json(req.params.id)
+  const store = await Store.findOne({ _id: req.params.id })
+  
+  // 2. Confirm they are the owner of the store
+  // TODO Implment login/logout
+  
+  // 3. Render the edit form so that the user can update their store
+  res.render('addEditStore', { title: 'Edit store', store })
+}
+
+exports.updateStore = async (req, res) => {
+  // 1. Find and update the store
+  const store = await Store.findOneAndUpdate(
+    { _id: req.params.id }, 
+    req.body, 
+    { new: true, runValidators: true }).exec()
+
+  req.flash('success', `Successfully updated ${store.name}. <a href="/stores/${store.slug}">View Store</a>`)
+
+  // 2. Redirect user to the store and tell them it worked
+  res.redirect(`/stores/${store._id}/edit`)
+}
--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,9 @@ const { catchErrors } = require('../handlers/errorHandlers')
 router.get('/', catchErrors(storeController.getStores))
 router.get('/stores', catchErrors(storeController.getStores))
 router.get('/add', storeController.addStore)
+router.get('/stores/:id/edit', catchErrors(storeController.editStore))
 
 router.post('/add', catchErrors(storeController.createStore))
+router.post('/add/:id', catchErrors(storeController.updateStore))
 
 module.exports = router;
