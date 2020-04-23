7a11b091e1f1918c9fe860d61188b9d4bccb72d7
Steve Poll <stevepoll@gmail.com>
2020-04-23 07:46:53 -0400

    Completed Lesson 23 - Creating User Accounts

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const storeController = require('../controllers/storeController')
+const userController = require('../controllers/userController')
 
 const { catchErrors } = require('../handlers/errorHandlers')
 
@@ -24,5 +25,13 @@ router.post('/add/:id',
   catchErrors(storeController.resize), 
   catchErrors(storeController.updateStore)
 )
+
+router.get('/login', userController.loginForm)
+router.get('/register', userController.registerForm)
+
+// 1. validate registration data
+// 2. register the user
+// 3. log them in
+router.post('/register', userController.validateRegister)
 
 module.exports = router;
