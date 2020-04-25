7c64921c388852e3b3acf9fbb91ab157629ee3d9
Steve Poll <stevepoll@gmail.com>
2020-04-25 11:24:24 -0400

    Completed Lesson 33 - Creating a Geospatial Ajax Endpoint

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -112,6 +112,26 @@ exports.searchStores = async (req, res) => {
   )
   .sort({ score: { $meta: 'textScore' } })
   .limit(5)
-  
+
+  res.json(stores)
+}
+
+exports.mapStores = async (req, res) => {
+  const coordinates = [req.query.lng, req.query.lat].map(parseFloat)
+  const q = {
+    location: {
+      $near: {
+        $geometry: {
+          type: 'Point',
+          coordinates
+        },
+        $maxDistance: 10000 // 10 km
+      }
+    }
+  }
+
+  const stores = await Store.find(q)
+    .select('slug name description location')
+    .limit(10)
   res.json(stores)
 }
